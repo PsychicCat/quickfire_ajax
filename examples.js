c830b5958f1699a1af0809b64ce56eeb629912e3
Brendan Telzrow <telzrowb@gmail.com>
2015-08-21 10:29:22 -0500

    initial commit

---
+++ examples.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+	var callback = function(response){
+	
+		response.forEach(function(elem){
+			console.log(elem);
+			$div = $('<div>');
+			$p = $('<p>');
+			$div.css('background-color', elem.value);
+			$p.text(elem.color);
+			$div.html($p);
+			$('.container').append($div);
+		});
+	}
+
+
+
+    $.ajax('data.json', {
+        success: callback,
+        error: function(request, errorType, errorMessage){
+            alert(errorType + " " + errorMessage);
+        }
+    });
+
+
+
+})
