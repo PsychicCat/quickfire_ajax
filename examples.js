be453b5ff0b1be9002a359a77033632425203aed
Brendan Telzrow <telzrowb@gmail.com>
2015-08-21 10:49:08 -0500

    updated for handlebars

--- examples.js
+++ examples.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
 
+
+
 	var callback = function(response){
-	
-		response.forEach(function(elem){
-			console.log(elem);
-			$div = $('<div>');
-			$p = $('<p>');
-			$div.css('background-color', elem.value);
-			$p.text(elem.color);
-			$div.html($p);
-			$('.container').append($div);
-		});
+	var source   = $("#Ajax-template").html();
+	var template = Handlebars.compile(source);
+	$('.container').html(template(response));
+
+		// response.forEach(function(elem){
+		// 	console.log(elem);
+		// 	$div = $('<div>');
+		// 	$p = $('<p>');
+		// 	$div.css('background-color', elem.value);
+		// 	$p.text(elem.color);
+		// 	$div.html($p);
+		// 	$('.container').append($div);
+		// });
 	}
 
 
